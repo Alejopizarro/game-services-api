faf17053dcd97267b3753f6a3f1848223bede04b
Alejopizarro <alejomkt10@gmail.com>
2024-12-11 13:34:19 +0100

    Arquitectura basica del proyecto

---
+++ src/main/java/com/example/game/services/api/GameServicesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.game.services.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServicesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameServicesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/game/services/api/GameServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game.services.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
